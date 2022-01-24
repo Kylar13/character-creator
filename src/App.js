d1dfafbd127fbf9279d5cd0a65290152d1d71582
Kylar13 <ramonpans13@gmail.com>
2022-01-24 18:51:07 +0100

    feat: Solved exercises 1 through 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          backgroundColor: "inherit",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: "60%",
+        }}
+      />
+      <div
+        style={{
+          backgroundColor: "hsl(195deg, 20%, 86%)",
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          height: "40%",
+        }}
+      />
       <CharacterEditor />
       <Footer />
     </>
